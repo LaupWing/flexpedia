a44ae0fb2ae011ad1d3472eb12f04e7e69d93a25
LaupWing <loc_strict@hotmail.com>
2023-06-14 12:17:38 +0200

    getDefaultLocale

--- src/stores/translation.ts
+++ src/stores/translation.ts
@@ -1,13 +1,28 @@
-import { ref, computed } from "vue"
+import { ref } from "vue"
 import { defineStore } from "pinia"
+// @ts-ignore
+import i18n from "vue-i18n"
 
 export const useTranslationStore = defineStore("translation", () => {
-   const lang = ref(0)
+   const lang = ref<string>("")
    
    const getSupportedLocales = () => {
       return import.meta.env.VITE_SUPPORTED_LOCALES.split(",")
    }
 
+   const getDefaultLocale = () => {
+      return import.meta.env.VITE_DEFAULT_LOCALE
+   }
 
-   return { lang, getSupportedLocales }
+   const guessDefaultLocale = () => {
+
+   }
+
+   const switchLocale = (isEng: boolean) => {
+      const newLocale = isEng ? "en" : "nl"
+      document.querySelector("html")?.setAttribute("lang", newLocale)
+      localStorage.setItem("user-locale", newLocale)
+   }  
+
+   return { lang, getSupportedLocales, switchLocale, getDefaultLocale }
 })
