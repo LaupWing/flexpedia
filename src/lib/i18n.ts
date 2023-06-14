e5e36dea8d32519d3f32cb5b3b0750ced2a83658
LaupWing <loc_strict@hotmail.com>
2023-06-14 11:40:35 +0200

    update

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -1,6 +1,14 @@
+import en from "../translations/en.json"
+import nl from "../translations/nl.json"
 // @ts-ignore
 import { createI18n } from "vue-i18n"
 
 export default createI18n({
-   locale: import.meta.env.DEFAULT_LOCALE,
+   locale: import.meta.env.VITE_DEFAULT_LOCALE,
+   fallbackLocale: import.meta.env.VITE_FALLBACK_LOCALE,
+   legacy: false,
+   messages:{
+      en,
+      nl
+   }
 })
