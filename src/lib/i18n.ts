75f1783dee74898d8f8926d93b91539e0b34ec93
LaupWing <loc_strict@hotmail.com>
2023-06-14 10:57:28 +0200

    udpate

---
+++ src/lib/i18n.ts
@@ -0,0 +1,6 @@
+// @ts-ignore
+import { createI18n } from "vue-i18n"
+
+export default createI18n({
+   locale: import.meta.env.DEFAULT_LOCALE,
+})
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import "./assets/main.css"
+import "./lib/i18n"
 
 import { createApp } from "vue"
 import { createPinia } from "pinia"
