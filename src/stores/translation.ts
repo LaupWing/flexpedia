581c5e005335f4c7dbd36dd17e1ddbdad77abb52
LaupWing <loc_strict@hotmail.com>
2023-06-14 12:22:01 +0200

    getPersistedLocale

--- src/stores/translation.ts
+++ src/stores/translation.ts
@@ -7,15 +7,32 @@ export const useTranslationStore = defineStore("translation", () => {
    const lang = ref<string>("")
    
    const getSupportedLocales = () => {
-      return import.meta.env.VITE_SUPPORTED_LOCALES.split(",")
+      return import.meta.env.VITE_SUPPORTED_LOCALES.split(",") as Array<string>
    }
 
    const getDefaultLocale = () => {
       return import.meta.env.VITE_DEFAULT_LOCALE
    }
 
+   const isSupported = (locale: string) => {
+      return getSupportedLocales().includes(locale)
+   }
+
    const guessDefaultLocale = () => {
 
+   }
+
+   const getPersistedLocale = () => {
+      const persistedLocale = localStorage.getItem("user-locale")
+      
+      if(!persistedLocale){
+         return null
+      }
+      if(isSupported(persistedLocale)){
+         return persistedLocale
+      } else {
+         return null
+      }
    }
 
    const switchLocale = (isEng: boolean) => {
@@ -24,5 +41,12 @@ export const useTranslationStore = defineStore("translation", () => {
       localStorage.setItem("user-locale", newLocale)
    }  
 
-   return { lang, getSupportedLocales, switchLocale, getDefaultLocale }
+   return { 
+      lang, 
+      getSupportedLocales, 
+      switchLocale, 
+      getDefaultLocale,
+      getPersistedLocale,
+      isSupported
+   }
 })
