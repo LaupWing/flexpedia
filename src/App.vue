017071551fb86f2c479062abca25a35ed2b90dda
LaupWing <loc_strict@hotmail.com>
2023-06-14 11:18:25 +0200

    added toggle

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,19 @@
 <template>
    <header class="flex flex-col text-main">
       <div class="px-4 py-2">
-         <img 
-            class="w-20" 
-            src="https://www.flexpedia.nl/wp-content/uploads/2022/05/logo.svg" 
-            alt=""
-         >
+         <img class="w-20" src="https://www.flexpedia.nl/wp-content/uploads/2022/05/logo.svg" alt="">
       </div>
       <nav class="flex flex-col my-auto gap-y-10 text-sm">
-         <RouterLink 
-            class="flex flex-col items-center gap-y-1 relative py-2 justify-center opacity-30"
-            v-for="(link, index) in links"
-            :key="index"
-            :to="link.href"
-         >
-            <component :is="link.icon"/>
+         <RouterLink class="flex flex-col items-center gap-y-1 relative py-2 justify-center opacity-30" v-for="(link, index) in links" :key="index" :to="link.href">
+            <component :is="link.icon" />
             {{ link.text }}
          </RouterLink>
       </nav>
+      <label class="relative inline-flex items-center cursor-pointer mx-auto mb-6">
+         <input type="checkbox" value="" class="sr-only peer">
+         <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-400"></div>
+         <span class="ml-3 text-sm font-medium text-main">EN</span>
+      </label>
    </header>
    <div class="flex-1 p-4 pl-0 flex">
       <RouterView />
@@ -31,33 +27,34 @@ import IconHome from "./components/icons/IconHome.vue"
 import IconInfo from "./components/icons/IconInfo.vue"
 import IconLock from "./components/icons/IconLock.vue"
 
-const links:Array<{
+const links: Array<{
    href: string,
    icon: any,
    text: string
 }> = [
-   {
-      href: "/",
-      icon: IconHome,
-      text: "Home"
-   },
-   {
-      href: "/about",
-      icon: IconInfo,
-      text: "About"
-   },
-   {
-      href: "/secret",
-      icon: IconLock,
-      text: "Secret"
-   }
-]
+      {
+         href: "/",
+         icon: IconHome,
+         text: "Home"
+      },
+      {
+         href: "/about",
+         icon: IconInfo,
+         text: "About"
+      },
+      {
+         href: "/secret",
+         icon: IconLock,
+         text: "Secret"
+      }
+   ]
 </script>
 
 <style scoped>
 .router-link-active {
-   opacity: 100%!important;
+   opacity: 100% !important;
 }
+
 .router-link-active::after {
    content: "";
    width: 5px;
